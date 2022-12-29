7812f1f8524d8c591d29720113285bee78d3e1e6
SimaritS <simarits07+gh@gmail.com>
2022-12-29 15:36:17 -0800

    Full Solution For Islanded Maze
    -Solved the testMaze in 1515 moves
    -Takes around 3 minutes and 15 seconds

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,48 +1,108 @@
-// Solution for Un-islanded maze.
+// Solution for Islanded maze.
 // Author: SimaritS.
 
 #include "micromouseserver.h"
+#include <iostream>
+using namespace std;
 
 //Initializing the variable
 int rtCount = 0;
+int totCount = 0;
 
-void microMouseServer::studentAI()
-{
-    //Store the wall positions.
+//Initialize array for node marking.
+int visitArray[MAZE_HEIGHT][MAZE_WIDTH] = { 0 };
+
+void microMouseServer::studentAI() {
+    //Store the wall & mouse positions.
     bool wL = isWallLeft();
     bool wR = isWallRight();
     bool wF = isWallForward();
+    int posX = (this->maze->mouseX() - 1);
+    int posY = (this->maze->mouseY() - 1);
 
-    // Walls on all 3 sides. Deadend ahead. Turn back.
+    // Walls on all 3 sides. Deadend. Turn back. 111
     if (wL && wF && wR) {
         turnLeft();
         turnLeft();
         rtCount=0;
     }
 
-    // Right turn only. Turn right.
+    // Right turn only. Turn right. 110
     if (wL && wF && !wR) {
         turnRight();
         rtCount++;
     }
 
-    // If no wall on Left. Always turn left.
-    if (!wL) {
+    // Left turn only. Turn left. 011
+    if (!wL && wF && wR) {
         turnLeft();
         rtCount=0;
     }
 
-    //If wall on left & no wall in front no change.
-    if (wL && !wF){
+    // No wall in front no change. 101
+    if (wL && !wF && wR) {
         rtCount=0;
+    }
+
+    // No wall in F and R. 100
+    if (wL && !wF && !wR) {
+        rtCount=0;
+        if(visitArray[posX][posY] > 0) {
+            for(int i=1; i < (visitArray[posX][posY])%4; i++){
+                turnRight();
+            }
+        }
+        //Mark node
+        visitArray[posX][posY]++;
+    }
+
+    //If no wall on Left. Always turn left. 001
+    if ( (!wL && !wF && wR)) {
+        turnLeft();
+        rtCount=0;
+        if(visitArray[posX][posY] > 0) {
+            for(int i=1; i < (visitArray[posX][posY])%4; i++){
+                turnRight();
+            }
+        }
+        //Mark node
+        visitArray[posX][posY]++;
+    }
+
+    //If no wall on Left. Always turn left. 010
+    if ((!wL && wF && !wR)) {
+        turnLeft();
+        rtCount=0;
+        if(visitArray[posX][posY] > 0) {
+            for(int i=1; i < (visitArray[posX][posY])%4; i++){
+                turnRight();
+            }
+        }
+        //Mark node
+        visitArray[posX][posY]++;
+    }
+
+    //No wall. 000
+    if ((!wL && !wF && !wR)) {
+        turnLeft();
+        rtCount=0;
+        if(visitArray[posX][posY] > 0) {
+            for(int i=1; i < (visitArray[posX][posY])%4; i++){
+                turnRight();
+            }
+        }
+        //Mark node
+        visitArray[posX][posY]++;
     }
 
     //Mouse direction already set, Move fwd.
     moveForward();
 
     //The right turn counter. When it reaches 3 the mouse should stop.
-    if(rtCount==3){
+    if(rtCount==3) {
        printUI("Congratulations! You've reached the end!");
        foundFinish();
-    }    
+    }
+
+    cout << "Total Mouse Moves:" << totCount++ <<endl;
 }
